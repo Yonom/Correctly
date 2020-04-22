8a675db4aed28cd6ecf861f9bda31f46eed34f30
SimonBusse <33378400+SimonBusse@users.noreply.github.com>
2020-04-22 17:50:15 +0200

    Workshop Aufgabe1 Simon Busse (#16)
    * Created my Page and added it to navigation
    
    Created my Page and added it to navigation
    
    * fixed redirection error
    
    Co-authored-by: YannickLehr <33485945+YannickLehr@users.noreply.github.com>
    Co-authored-by: Simon Farshid <Yonom@users.noreply.github.com>

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ export default ({ current }) => {
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
       <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
+      <Link href="/seiten/aufgabeWorkshop1/pageSimonBusse"><span style={{ fontWeight: current == 'pageSimonBusse' ? 'bold' : 'normal' }}>Simon Busse</span></Link>
     </div>
   )
 }
