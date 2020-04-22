18627331d7c841cf79afa38309ad8d115081c2b2
LucaLenhard <33602680+LucaLenhard@users.noreply.github.com>
2020-04-22 18:22:02 +0200

    Workshop1 (#11)
    Luca Lenhard
    
    Co-authored-by: YannickLehr <33485945+YannickLehr@users.noreply.github.com>

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ export default ({ current }) => {
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
       <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
+      <Link href="/seiten/lucalenhard"><span style={{ fontWeight: current == 'lucalenhard' ? 'bold' : 'normal' }}>Workshop 1</span></Link>
       <Link href="/seiten/aufgabeWorkshop1/pageLinn"><span style={{ fontWeight: current == 'pageLinn' ? 'bold' : 'normal' }} >Linn</span></Link>
       <Link href="/aufgabeWorkshop1/pagecarl"><span style={{ fontWeight: current == 'carl' ? 'bold' : 'normal' }}>Carl</span></Link>
       <Link href="/seiten/aufgabeWorkshop1/pageLucaSteingen"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }}>Luca</span></Link>
