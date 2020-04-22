41d9b8651b0b4f24a1ec0babb99cfe7644ae8917
Malte Blank <malte.blank@fs-students.de>
2020-04-22 11:07:28 +0200

    workshop aufgabe 1 malte blank

---
+++ src/pages/pageMalteBlank.js
@@ -0,0 +1,32 @@
+import Navigation from "../../components/Navigation";
+
+export default () => {
+
+  //returns a random hexadecimal code of a color
+  function tempColorFunction() 
+  {
+   var randomColor = '#'+ Math.floor(Math.random()*16777215).toString(16);
+   return randomColor;
+  }
+  
+  //prints my name in random colors
+  function OutputFunction() 
+  {
+    let Output = []
+    for(let i = 1; i <= 100; i++)
+    {
+      let tempColor = tempColorFunction()
+        Output.push(<p style={{color: tempColor}}>Malte</p>)
+    };
+  return Output
+  }
+
+  return(
+    <div>
+      <Navigation current="pageMalteBlank" />
+      <h1> This is a header in h1 </h1>
+      <p id="Output">{ OutputFunction() }</p>
+    </div>
+  );
+
+};
