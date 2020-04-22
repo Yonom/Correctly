e8cc91e4d3ec539a3e2edc445d564962a05cec33
malteblank <33621463+malteblank@users.noreply.github.com>
2020-04-22 18:37:08 +0200

    Workshop aufgabe1 malte blank (#14)
    * workshop aufgabe 1 malte blank
    
    * Update pageMalteBlank.js
    
    * Bug Fix
    
    * Update Navigation.js
    
    Co-authored-by: Malte Blank <malte.blank@fs-students.de>
    Co-authored-by: YannickAaron <33485945+YannickLehr@users.noreply.github.com>
    Co-authored-by: Simon Farshid <Yonom@users.noreply.github.com>

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ export default ({ current }) => {
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
       <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
+      <Link href="/seiten/pageMalteBlank"><span style={{ fontWeight: current == 'pageMalteBlank' ? 'bold' : 'normal' }}>pageMalteBlank</span></Link>
       <Link href="/aufgabeWorkshop1/pageYannick"><span style={ current == 'yannick' ? { fontWeight:'bold', color: 'red',fontSize:"20px"} : { fontWeight:'normal', color: 'black', fontSize:"16px"} }>Yannick</span></Link>
       <Link href="/aufgabeWorkshop1/pageRobinRinn"><span style={{ fontWeight: current == 'pageRobinRinn' ? 'bold' : 'normal' }}>pageRobinRinn</span></Link>
       <Link href="/seiten/lucalenhard"><span style={{ fontWeight: current == 'lucalenhard' ? 'bold' : 'normal' }}>Workshop 1</span></Link>
