5c4cc5652355fad36d5231a0132fd7146710e679
rmr23 <robrinn@web.de>
2020-04-22 18:17:46 +0200

    Workshop1 task compled

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ export default ({ current }) => {
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
       <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
+      <Link href="/aufgabeWorkshop1/pageRobinRinn"><span style={{ fontWeight: current == 'pageRobinRinn' ? 'bold' : 'normal' }}>pageRobinRinn</span></Link>
     </div>
   )
 }
