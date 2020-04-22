8937b03cfc76ff6f1229c404d4c6d76bebea9be7
YannickAaron <33485945+YannickLehr@users.noreply.github.com>
2020-04-22 18:34:44 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ export default ({ current }) => {
   return (
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
+      <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
       <Link href="/seiten/pageMalteBlank"><span style={{ fontWeight: current == 'pageMalteBlank' ? 'bold' : 'normal' }}>pageMalteBlank</span></Link>
     </div>
   )
