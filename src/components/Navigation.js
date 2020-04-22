e933457e98c8cf5bea88d8c98b2a2260eee8293f
YannickAaron <33485945+YannickLehr@users.noreply.github.com>
2020-04-22 10:51:24 +0200

    Aufgabe 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ export default ({ current }) => {
     <div>
       <Link href="/"><span style={{ fontWeight: current == 'index' ? 'bold' : 'normal' }} >Home</span></Link>
       <Link href="/seiten/seite2"><span style={{ fontWeight: current == 'seite2' ? 'bold' : 'normal' }}>Seite 2</span></Link>
+      <Link href="/aufgabeWorkshop1/pageYannick"><span style={ current == 'yannick' ? { fontWeight:'bold', color: 'red',fontSize:"20px"} : { fontWeight:'normal', color: 'black', fontSize:"16px"} }>Yannick</span></Link>
     </div>
   )
 }
