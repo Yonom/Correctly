66bbd92e0d83ce721c3921c5618ee20d9d2fda84
Simon Farshid <Yonom@users.noreply.github.com>
2020-04-23 13:23:48 +0200

    Add databaseTransaction

--- src/database/index.js
+++ src/database/index.js
@@ -28,3 +28,22 @@ export const databaseQuery = async (text, params = undefined) => {
   return await pool.query(text, params);
 };
 
+/**
+ * Starts a transaction and calls the callback function.
+ * If the callback executes without exceptions, the transaction is commited, otherwise rolled back.
+ *
+ * @param {Function<import('pg').PoolClient> | Function<Promise<import('pg').PoolClient>>} callback The callback to be called.
+ */
+export const databaseTransaction = async (callback) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    await callback(client);
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+};
