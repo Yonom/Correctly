3a0a1da700480c1dde76315464f10c53566eee65
Simon Farshid <Yonom@users.noreply.github.com>
2020-04-25 12:24:15 +0200

    Add firebase token verification support

--- src/utils/api/auth/tokenJWT.js
+++ src/utils/api/auth/tokenJWT.js
@@ -1,10 +1,17 @@
 
 import jwt from 'jsonwebtoken';
+import { auth as firebaseAuth } from 'firebase-admin';
 import { jwt as jwtKey } from '../../../../.keys/key.json';
+
+import { firebaseConfig } from '../../../clientConfig';
 
 const TOKEN_EXPIRY = '1h';
 
 const auth = firebaseAuth(firebaseConfig.appId);
+
+export const verifyFirebaseToken = (token) => {
+  return auth.verifyIdToken(token);
+};
 
 export const generateToken = (userId) => {
   return new Promise((resolve, reject) => {
