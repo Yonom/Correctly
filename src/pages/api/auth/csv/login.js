05222c083c1a58537f9b18dc58082619952458c5
Simon Farshid <Yonom@users.noreply.github.com>
2020-04-25 14:56:12 +0200

    Format with ESLint

--- src/pages/api/auth/csv/login.js
+++ src/pages/api/auth/csv/login.js
@@ -1,56 +1,41 @@
-//API erwartet einen POST-Request im JSON-Format mit den Attributen
-//email und password
+import csvParser from 'csv-parser';
+import fs from 'fs';
+
+const csvFilepath = '.keys/users.csv';
+
+// API erwartet einen POST-Request im JSON-Format mit den Attributen
+// email und password
 export default async (req, res) => {
-    
-    //Prüfung auf POST-Request
-    if (req.method === 'POST') {
+  // Prüfung auf POST-Request
+  if (req.method === 'POST') {
+    const csvArray = [];
 
-        //CSV-Parser wird installiert
-        const csvParser = require('csv-parser');
-        const fs = require('fs');
-        const csv_array = [];
+    // users.csv Pfad wird definiert und foundUser auf false gesetzt
+    let foundUser = false;
 
-        //users.csv Pfad wird definiert und foundUser auf false gesetzt
-        const csv_filepath = ".keys/users.csv";
-        let foundUser = false;
+    const givenEmail = req.body.email;
+    const givenPassword = req.body.password;
 
-
-        const givenEmail = req.body.email;
-        const givenPassword = req.body.password;
-    
-        
-        fs.createReadStream(csv_filepath)
-        .pipe(csvParser())
-        .on('data', (data) => csv_array.push(data))
-        .on('end', () => {   
-        //users.csv wird Case-Insensitive nach email durchsucht
-        for (let i of csv_array){
-            if(i.email.toLowerCase().localeCompare(givenEmail.toLowerCase())===0) {
-                //falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und 
-                //foundUser auf true gesetzt
-                if(i.password.localeCompare(givenPassword,'de')===0) {
-                    foundUser = true;
-                    
-                }
+    fs.createReadStream(csvFilepath)
+      .pipe(csvParser())
+      .on('data', (data) => csvArray.push(data))
+      .on('end', () => {
+        // users.csv wird Case-Insensitive nach email durchsucht
+        for (const i of csvArray) {
+          if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase()) === 0) {
+            // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
+            // foundUser auf true gesetzt
+            if (i.password.localeCompare(givenPassword, 'de') === 0) {
+              foundUser = true;
             }
+          }
         }
 
-        //foundUser wird als JSON response zurückgegeben
+        // foundUser wird als JSON response zurückgegeben
         res.status(200)(foundUser);
-        
-    });
-  
+      });
+  } else {
+    // Antwort falls Request keine POST Methode ist
+    res.status(400).json({ });
   }
-  //Antwort falls Request keine POST Methode ist
-  else
-  {
-    res.status(400).json(
-        {
-            
-        }
-    )
-  }
-
-}
-
-
+};
