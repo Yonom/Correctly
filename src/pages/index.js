26cd88c0305726da85cb9e8a096e35956c823243
YannickAaron <33485945+YannickLehr@users.noreply.github.com>
2020-04-26 13:19:22 +0200

    addLoginPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,31 @@
+import {
+  IonButton, IonIcon, IonContent, IonLabel, IonItem, IonList, IonInput, IonText,
+} from '@ionic/react';
+
 export default () => {
   return (
     <div>
-      <p>Hello world</p>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Login-Seite</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <IonContent fullscreen>
+        <IonList lines="full" class="ion-no-margin ion-no-padding">
+          <IonItem>
+            <IonLabel position="stacked">Email-Adresse  <IonText color="danger">*</IonText></IonLabel>
+            <IonInput required type="text" oninput="handleFirstNameValue(event)" />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Passwort <IonText color="danger">*</IonText></IonLabel>
+            <IonInput required type="text" oninput="handleFirstNameValue(event)" />
+          </IonItem>
+        </IonList>
+        <div className="ion-padding">
+          <IonButton expand="block" type="submit" class="ion-no-margin">Anmelden</IonButton>
+        </div>
+      </IonContent>
     </div>
   );
 };
