266bbc23e76587a80bbf5d7c2c305b7ec8c48ad4
rmr23 <robrinn@web.de>
2020-04-26 15:16:02 +0200

    Basic Update/Insert 'User' into DB Functionality

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,4 @@
+
 import { Pool } from 'pg';
 import key from '../../.keys/key.json';
 
@@ -16,4 +17,55 @@ const pool = new Pool(config);
 export const databaseTest = async () => {
   const client = await pool.connect();
   client.release();
+};
+
+/**
+ * Returns the users in the database for debugging purposes.
+ * Should be deleted later.
+ */
+export const printUserEntries = async () => {
+  const queryText = 'SELECT * FROM users';
+  const maxNumber = 20;
+  pool
+  .query(queryText,)
+  .then(res => {
+    for (let i = 0; i < res.rows.length && i< maxNumber; i++) {
+    console.log("row: " + String(i) + ": ", res.rows[i]);
+  }
+  })
+  .catch(err =>
+    setImmediate(() => {
+      throw err
+    })
+  );
+}
+
+/**
+ * Runs a single query.
+ *
+ * @param {string} text The query to run.
+ * @param {object<string, string>} params The parameters to insert into the query.
+ */
+export const databaseQuery = async (text, params = undefined) => {
+  return await pool.query(text, params);
+};
+
+/**
+ * Starts a transaction and calls the callback function.
+ * If the callback executes without exceptions, the transaction is commited, otherwise rolled back.
+ *
+ * @param {Function<import('pg').PoolClient> | Function<Promise<import('pg').PoolClient>>} callback The callback to be called.
+ */
+export const databaseTransaction = async (callback) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    await callback(client);
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
 };
