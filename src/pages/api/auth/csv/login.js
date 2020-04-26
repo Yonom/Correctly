f87fb33a09c6f9f2af1e9ac28ffeb56689ed2f42
Simon Farshid <Yonom@users.noreply.github.com>
2020-04-26 15:05:54 +0200

    Use neat-csv for an async API

--- src/pages/api/auth/csv/login.js
+++ src/pages/api/auth/csv/login.js
@@ -1,4 +1,4 @@
-import csvParser from 'csv-parser';
+import csvParser from 'neat-csv';
 import fs from 'fs';
 
 const csvFilepath = '.keys/users.csv';
@@ -8,34 +8,28 @@ const csvFilepath = '.keys/users.csv';
 export default async (req, res) => {
   // Prüfung auf POST-Request
   if (req.method === 'POST') {
-    const csvArray = [];
-
     // users.csv Pfad wird definiert und foundUser auf false gesetzt
     let foundUser = false;
 
     const givenEmail = req.body.email;
     const givenPassword = req.body.password;
 
-    fs.createReadStream(csvFilepath)
-      .pipe(csvParser())
-      .on('data', (data) => csvArray.push(data))
-      .on('end', () => {
-        // users.csv wird Case-Insensitive nach email durchsucht
-        for (const i of csvArray) {
-          if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase()) === 0) {
-            // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
-            // foundUser auf true gesetzt
-            if (i.password.localeCompare(givenPassword, 'de') === 0) {
-              foundUser = true;
-            }
-          }
+    const csvArray = await csvParser(await fs.promises.readFile(csvFilepath));
+    // users.csv wird Case-Insensitive nach email durchsucht
+    for (const i of csvArray) {
+      if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase()) === 0) {
+        // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
+        // foundUser auf true gesetzt
+        if (i.password.localeCompare(givenPassword, 'de') === 0) {
+          foundUser = true;
         }
+      }
+    }
 
-        // foundUser wird als JSON response zurückgegeben
-        res.status(200).json({
-          csv_login: foundUser,
-        });
-      });
+    // foundUser wird als JSON response zurückgegeben
+    res.status(200).json({
+      csv_login: foundUser,
+    });
   } else {
     // Antwort falls Request keine POST Methode ist
     res.status(400).json({ });
