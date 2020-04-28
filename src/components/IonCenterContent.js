43d61a312bf10a84ceec1e663a11f8988f292f57
YannickAaron <33485945+YannickLehr@users.noreply.github.com>
2020-04-28 19:00:36 +0200

    makePraxisprojektGreatAgain
    Resolved all issues

--- src/components/IonCenterContent.js
+++ src/components/IonCenterContent.js
@@ -1,10 +1,10 @@
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
 
-export default ({ children, contentPadding }) => {
+export default ({ children, innerStyle }) => {
   return (
     <IonGrid style={{ height: '100%' }}>
       <IonRow style={{ height: '100%' }}>
-        <IonCol class="ion-align-self-center" style={{ padding: contentPadding }}>
+        <IonCol class="ion-align-self-center" style={innerStyle}>
           {children}
         </IonCol>
       </IonRow>
--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,12 +1,9 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-
 /* Ionic imports */
 import { IonButton, IonContent, IonLabel, IonItem, IonList, IonInput, IonText, IonAlert } from '@ionic/react';
 
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 /* Custom components */
 import AppPage from '../../components/AppPage';
@@ -17,7 +14,6 @@ import IonCenterContent from '../../components/IonCenterContent';
 import { login } from '../../services/auth';
 
 export default () => {
-  const router = useRouter();
   const [showLoginErrorAlert, setShowLoginErrorAlert] = useState(false);
 
   /* executes the login function from '../../services/auth' and triggers an error message if an exception occures */
@@ -38,7 +34,7 @@ export default () => {
   return (
     <AppPage title="Login Seite" footer="Correctly">
       <IonContent>
-        <IonCenterContent contentPadding="10%">
+        <IonCenterContent innerStyle={{ padding: '10%' }}>
           <form onSubmit={handleSubmit(onSubmit)}>
             <IonList lines="full" class="ion-no-margin ion-no-padding">
               <IonItem>
@@ -53,22 +49,23 @@ export default () => {
             <div className="ion-padding">
               <IonButton type="submit" expand="block" class="ion-no-margin">Anmelden</IonButton>
             </div>
-            <div className="ion-padding">
-              <IonText>Probleme bei der Anmeldung? <Link href="../"><a>Passwort vergessen</a></Link>
-              </IonText>
-            </div>
-            <section className="full-width">
-              <IonButton onClick={() => router.push('../')} expand="full" color="secondary">Zur Registrierung</IonButton>
-            </section>
-            <IonAlert
-              isOpen={showLoginErrorAlert}
-              onDidDismiss={() => setShowLoginErrorAlert(false)}
-              header="Falsche Login-Daten"
-              subHeader="Passwort falsch, oder Nutzer nicht gefunden."
-              message=""
-              buttons={['OK']}
-            />
           </form>
+          <div className="ion-padding">
+            <IonText>Probleme bei der Anmeldung? <Link href="../"><a>Passwort vergessen</a></Link>
+            </IonText>
+          </div>
+          <section className="full-width">
+            <Link href="../"><a><IonButton expand="full" color="secondary">Zur Registrierung</IonButton></a></Link>
+          </section>
+          <IonAlert
+            isOpen={showLoginErrorAlert}
+            onDidDismiss={() => setShowLoginErrorAlert(false)}
+            header="Falsche Login-Daten"
+            subHeader="Passwort falsch, oder Nutzer nicht gefunden."
+            message=""
+            buttons={['OK']}
+          />
+
         </IonCenterContent>
       </IonContent>
     </AppPage>
