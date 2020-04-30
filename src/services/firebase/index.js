25012a8e98ee77f1b0e617b4149d003636ad4557
Simon Farshid <Yonom@users.noreply.github.com>
2020-04-30 23:44:07 +0200

    Add support for converting firebase errors to user readable messages
    TODO: Define messages

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-mutable-exports */
 import { initializeApp, apps, auth } from 'firebase/app';
 import 'firebase/auth';
-import { firebaseConfig } from '../utils/config';
+import { firebaseConfig } from '../../utils/config';
 
+/** @type {auth.Auth} */
 let firebaseAuth;
 if (typeof window !== 'undefined' && !apps.length) {
   initializeApp(firebaseConfig);
