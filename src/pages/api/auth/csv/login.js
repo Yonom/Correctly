727e50bd8fb6e4a5d1cc07a4cf0e47a5e2d67467
rmr23 <33514921+rmr23@users.noreply.github.com>
2020-05-04 19:45:14 +0200

    Change Requests
    - Request status setting in handler removed
    - Added Error trowhing in case of bad request

--- src/pages/api/auth/csv/login.js
+++ src/pages/api/auth/csv/login.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { generateToken } from '../../../../utils/api/auth/tokenJWT';
 import { setCookie } from '../../../../utils/api/auth/tokenCookie';
 import { authProvider } from '../../../../utils/config';
-import handleReq from '../../../../utils/api/handleReq';
+import handleRequestMethod from '../../../../utils/api/handleReq';
 import { upsertUser } from '../../../../services/api/database/user';
 
 const csvFilepath = '.keys/users.csv';
@@ -15,41 +15,38 @@ export default async (req, res) => {
     return res.status(400).json({ error: 'Server does not support csv login.' });
   }
   // Prüfung auf POST-Request
-  const handledRes = handleReq(req, res, 'POST');
-  if (handledRes.statusCode === 200) {
-    const givenEmail = req.body.email;
-    const givenPassword = req.body.password;
+  handleRequestMethod(req, res, 'POST');
 
-    const csvArray = await csvParser(await fs.promises.readFile(csvFilepath));
+  const givenEmail = req.body.email;
+  const givenPassword = req.body.password;
 
-    // users.csv wird Case-Insensitive nach email durchsucht
-    let foundUser;
-    for (const i of csvArray) {
-      if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase(), 'de') === 0) {
-        // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
-        // foundUser auf true gesetzt
-        if (i.password.localeCompare(givenPassword, 'de') === 0) {
-          foundUser = i;
-        }
+  const csvArray = await csvParser(await fs.promises.readFile(csvFilepath));
+
+  // users.csv wird Case-Insensitive nach email durchsucht
+  let foundUser;
+  for (const i of csvArray) {
+    if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase(), 'de') === 0) {
+      // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
+      // foundUser auf true gesetzt
+      if (i.password.localeCompare(givenPassword, 'de') === 0) {
+        foundUser = i;
       }
     }
-
-    // prüfung, ob ein Benutzer gefunden wurde
-    if (foundUser) {
-      // upsertUser aufrufen zur Synchronisation der Daten
-      upsertUser(foundUser.userId, foundUser.email, foundUser.firstName, foundUser.lastName, foundUser.studentId, foundUser.email_verified);
-
-      // Cookies setzen
-      setCookie(res, await generateToken(foundUser.userId), req.secure);
-
-      // 200 OK zurückgeben
-      return res.status(200).json({ });
-    }
-
-    // Benutzer nicht gefunden
-    // 403 Forbidden
-    return res.status(403).json({ error: 'Invalid credentials sent!' });
   }
-  // Antwort falls Request keine POST Methode ist
-  return handledRes.end();
+
+  // prüfung, ob ein Benutzer gefunden wurde
+  if (foundUser) {
+    // upsertUser aufrufen zur Synchronisation der Daten
+    upsertUser(foundUser.userId, foundUser.email, foundUser.firstName, foundUser.lastName, foundUser.studentId, foundUser.email_verified);
+
+    // Cookies setzen
+    setCookie(res, await generateToken(foundUser.userId), req.secure);
+
+    // 200 OK zurückgeben
+    return res.status(200).json({ });
+  }
+
+  // Benutzer nicht gefunden
+  // 403 Forbidden
+  return res.status(403).json({ error: 'Invalid credentials sent!' });
 };
--- src/utils/api/handleReq.js
+++ src/utils/api/handleReq.js
@@ -7,14 +7,16 @@
  * @param {object} req http request
  * @param {object} res http response
  * @param {string[]} allowedMethods array of allowed methods
- * @returns {object} http resonse 200 if okay and 405 if false method used
+ * @returns {boolean} true if okay and throws error if false
  */
-export default function handleReq(req, res, ...allowedMethods) {
+export default function handleRequestMethod(req, res, ...allowedMethods) {
   if (allowedMethods.includes(req.method)) {
     // Return 200 OK if an allowed method was used.
-    return res.status(200);
+    return true;
   }
   // Return 405 'Method Not Allowed' if anything except the
   // allowed methods is used
-  return res.writeHead(405, { Allow: allowedMethods });
+  // return res.writeHead(405, { Allow: allowedMethods });
+  res.writeHead(405, { Allow: allowedMethods }).end();
+  throw new Error('Wrong request method used.');
 }
