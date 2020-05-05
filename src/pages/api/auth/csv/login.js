75cd79a7c6da403a5b2bb9d34540159221562bda
rmr23 <33514921+rmr23@users.noreply.github.com>
2020-05-05 12:36:16 +0200

    Handler for correct request method added (#53)
    * Handler for correct request method added
    
    * Change Requests
    
    - Request status setting in handler removed
    - Added Error trowhing in case of bad request
    
    * call handleRequestMethod on firebase endpoints as well
    
    * Update swagger file
    
    * ESLint fix
    
    Co-authored-by: YannickLehr <33485945+YannickLehr@users.noreply.github.com>
    Co-authored-by: Simon Farshid <Yonom@users.noreply.github.com>

--- src/pages/api/auth/csv/login.js
+++ src/pages/api/auth/csv/login.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import { generateToken } from '../../../../utils/api/auth/tokenJWT';
 import { setCookie } from '../../../../utils/api/auth/tokenCookie';
 import { authProvider } from '../../../../utils/config';
+import handleRequestMethod from '../../../../utils/api/handleReq';
 import { upsertUser } from '../../../../services/api/database/user';
 
 const csvFilepath = '.keys/users.csv';
@@ -10,46 +11,43 @@ const csvFilepath = '.keys/users.csv';
 // API erwartet einen POST-Request im JSON-Format mit den Attributen
 // email und password
 export default async (req, res) => {
+  // Prüfung auf POST-Request
+  handleRequestMethod(req, res, 'POST');
+
   if (authProvider !== 'csv') {
     return res.status(400).json({ code: 'auth/csv-not-enabled' });
   }
 
-  // Prüfung auf POST-Request
-  if (req.method === 'POST') {
-    const givenEmail = req.body.email;
-    const givenPassword = req.body.password;
+  const givenEmail = req.body.email;
+  const givenPassword = req.body.password;
 
-    const csvArray = await csvParser(await fs.promises.readFile(csvFilepath));
+  const csvArray = await csvParser(await fs.promises.readFile(csvFilepath));
 
-    // users.csv wird Case-Insensitive nach email durchsucht
-    let foundUser;
-    for (const i of csvArray) {
-      if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase(), 'de') === 0) {
-        // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
-        // foundUser auf true gesetzt
-        if (i.password.localeCompare(givenPassword, 'de') === 0) {
-          foundUser = i;
-        }
+  // users.csv wird Case-Insensitive nach email durchsucht
+  let foundUser;
+  for (const i of csvArray) {
+    if (i.email.toLowerCase().localeCompare(givenEmail.toLowerCase(), 'de') === 0) {
+      // falls email gefunden wird, wird Case-Sensitive das Passwort verglichen und
+      // foundUser auf true gesetzt
+      if (i.password.localeCompare(givenPassword, 'de') === 0) {
+        foundUser = i;
       }
     }
-
-    // prüfung, ob ein Benutzer gefunden wurde
-    if (foundUser) {
-      // upsertUser aufrufen zur Synchronisation der Daten
-      await upsertUser(foundUser.userId, foundUser.email, foundUser.firstName, foundUser.lastName, foundUser.studentId, true);
-
-      // Cookies setzen
-      setCookie(res, await generateToken(foundUser.userId), req.secure);
-
-      // 200 OK zurückgeben
-      return res.status(200).json({ });
-    }
-
-    // Benutzer nicht gefunden
-    // 403 Forbidden
-    return res.status(403).json({ code: 'auth/wrong-password' });
   }
 
-  // Antwort falls Request keine POST Methode ist
-  return res.status(400).json({ });
+  // prüfung, ob ein Benutzer gefunden wurde
+  if (foundUser) {
+    // upsertUser aufrufen zur Synchronisation der Daten
+    upsertUser(foundUser.userId, foundUser.email, foundUser.firstName, foundUser.lastName, foundUser.studentId, true);
+
+    // Cookies setzen
+    setCookie(res, await generateToken(foundUser.userId), req.secure);
+
+    // 200 OK zurückgeben
+    return res.status(200).json({ });
+  }
+
+  // Benutzer nicht gefunden
+  // 403 Forbidden
+  return res.status(403).json({ code: 'auth/wrong-password' });
 };
