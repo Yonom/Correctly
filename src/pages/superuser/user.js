c15817b70d6df42c73cb0161c6d897d918a3e772
YannickAaron <33485945+YannickLehr@users.noreply.github.com>
2020-05-17 09:39:16 +0200

    draft

--- src/pages/superuser/user.js
+++ src/pages/superuser/user.js
@@ -1,5 +1,5 @@
 /* Ionic imports */
-import { IonButton, IonContent, IonLabel, IonItem, IonList, IonInput, IonText, IonSelect, IonDatetime, IonSelectOption, IonIcon, IonSearchbar, IonItemGroup, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { IonButton, IonContent, IonLabel, IonItem, IonList, IonInput, IonText, IonGrid, IonRow, IonSelect, IonDatetime, IonSelectOption, IonIcon, IonSearchbar, IonItemGroup, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
 
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -8,6 +8,7 @@ import { cloudUploadOutline } from 'ionicons/icons';
 
 /* Custom components */
 import AppPage from '../../components/AppPage';
+import Expandable from '../../components/Expandable';
 import IonController from '../../components/IonController';
 import IonCenterContent from '../../components/IonCenterContent';
 
@@ -19,52 +20,67 @@ export default () => {
   };
 
   return (
-    <AppPage title="User-Interface (Superuser" footer="Correctly">
-      <IonHeader>
+    <AppPage title="User-Interface (Superuser)" footer="Correctly">
+      <IonHeader translucent>
         <IonToolbar>
-          <h2>Nuzerdaten ändern</h2>
-          <IonSearchbar value="test" />
+          <IonSearchbar value="test" placeholder="Filter nach Name" />
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent fullscreen>
         <IonList>
-          <ion-item>Amsterdam</ion-item>
-          <ion-item>Bogota</ion-item>
-          <ion-item>Buenos Aires</ion-item>
-          <ion-item>Cairo</ion-item>
-          <ion-item>Dhaka</ion-item>
-          <ion-item>Edinburgh</ion-item>
-          <ion-item>Geneva</ion-item>
-          <ion-item>Genoa</ion-item>
-          <ion-item>Glasgow</ion-item>
-          <ion-item>Hanoi</ion-item>
-          <ion-item>Hong Kong</ion-item>
-          <ion-item>Islamabad</ion-item>
-          <ion-item>Istanbul</ion-item>
-          <ion-item>Jakarta</ion-item>
-          <ion-item>Kiel</ion-item>
-          <ion-item>Kyoto</ion-item>
-          <ion-item>Le Havre</ion-item>
-          <ion-item>Lebanon</ion-item>
-          <ion-item>Lhasa</ion-item>
-          <ion-item>Lima</ion-item>
-          <ion-item>London</ion-item>
-          <ion-item>Los Angeles</ion-item>
-          <ion-item>Madrid</ion-item>
-          <ion-item>Manila</ion-item>
-          <ion-item>New York</ion-item>
-          <ion-item>Olympia</ion-item>
-          <ion-item>Oslo</ion-item>
-          <ion-item>Panama City</ion-item>
-          <ion-item>Peking</ion-item>
-          <ion-item>Philadelphia</ion-item>
-          <ion-item>San Francisco</ion-item>
-          <ion-item>Seoul</ion-item>
-          <ion-item>Taipeh</ion-item>
-          <ion-item>Tel Aviv</ion-item>
-          <ion-item>Tokio</ion-item>
-          <ion-item>Uelzen</ion-item>
-          <ion-item>Washington</ion-item>
+          <IonItem>
+            <IonGrid>
+              <IonRow>
+                <IonLabel>
+                  <div>
+                    <h2>Max Hans Mustermann</h2>
+                    <h3>max_hans.mustermann@fs-students.de</h3>
+                  </div>
+                </IonLabel>
+              </IonRow>
+              <hr />
+              <IonRow>
+                <IonItem>
+                  <IonLabel position="stacked">
+                    Name
+                    {' '}
+                    <IonText color="danger">*</IonText>
+                  </IonLabel>
+                  <IonInput required type="text" />
+                </IonItem>
+              </IonRow>
+              <IonRow>
+                <IonItem>
+                  <IonLabel position="stacked">
+                    Vorname
+                    {' '}
+                    <IonText color="danger">*</IonText>
+                  </IonLabel>
+                  <IonInput required type="text" />
+                </IonItem>
+              </IonRow>
+              <IonRow>
+                <IonItem>
+                  <IonLabel position="stacked">
+                    Matrikelnummer
+                    {' '}
+                    <IonText color="danger">*</IonText>
+                  </IonLabel>
+                  <IonInput required type="text" />
+                </IonItem>
+              </IonRow>
+              <IonRow>
+                <IonItem>
+                  <IonLabel position="stacked">
+                    E-Mail
+                    {' '}
+                    <IonText color="danger">*</IonText>
+                  </IonLabel>
+                  <IonInput required type="text" />
+                </IonItem>
+              </IonRow>
+            </IonGrid>
+          </IonItem>
         </IonList>
       </IonContent>
     </AppPage>
