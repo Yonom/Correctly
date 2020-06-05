0e8caf2c7c3e47c72ffa828ebba34d721e62f4ee
Simon Farshid <Yonom@users.noreply.github.com>
2020-06-05 16:43:15 +0200

    Update test.js

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,7 @@
 import loadKey from '../../utils/api/loadKey';
 
 const fs = require('fs');
-const files = fs.readdirSync('/');
+const files = fs.readdirSync('/var/task');
 export default async (req, res) => {
   if (false) loadKey();
   res.json({
