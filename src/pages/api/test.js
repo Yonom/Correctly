869ee9c4651909c42da6c5910caa4cdd0461bd69
Simon Farshid <Yonom@users.noreply.github.com>
2020-06-05 16:41:24 +0200

    new test

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,13 @@
+import loadKey from '../../utils/api/loadKey';
+
+const fs = require('fs');
+const files = fs.readdirSync('/');
 export default async (req, res) => {
+  if (false) loadKey();
   res.json({
     __filename,
     __dirname,
+    files,
     cwd: process.cwd(),
   });
 };
