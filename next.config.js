009d79e1e5c69608bf22fc96f604840e0db3c567
Simon Farshid <Yonom@users.noreply.github.com>
2020-06-05 18:14:04 +0200

    Documentation

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // there are three modes available: dev, server, vercel (serverless)
-      // in the final app (dev and server modes), configuration data is loaded from the .keys folder
       // on vercel (serverless), the .keys folder is not properly copied to each serverless endpoint, so we implement a workaround here
+      // in the final app and during development, configuration data is loaded from the .keys folder
+      // also see src/utils/api/loadConfig.js
 
-      // dev / server mode: files in .keys folder will be imported as filepath
-      // vercel mode: files in .keys folder will be imported as content
+      // dev / normal build: configuration files are loaded at runtime from .keys folder and are dynamic
+      // vercel build: the configuration files are copied into the finished bundle and are static
 
       const isVercel = !!process.env.VERCEL_URL;
       config.module.rules.push({
