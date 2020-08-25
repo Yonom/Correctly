12404a13a51087e65a52c6844e94f26f18bea7de
Simon Farshid <Yonom@users.noreply.github.com>
2020-08-25 17:37:39 +0200

    require login for postman endpoint

--- src/pages/api/postman.js
+++ src/pages/api/postman.js
@@ -1,7 +1,10 @@
 import handleRequestMethod from '../../utils/api/handleRequestMethod';
+import requireLogin from '../../utils/api/auth/requireLogin';
 
 const debug = async (req, res) => {
   await handleRequestMethod(req, res, 'GET');
+  await requireLogin(req, res);
+  
   return res.json({ authCookie: `${res.getHeader('Set-Cookie').split(';')[0]}; Domain=${req.headers.host.split(':')[0]}; Path=/` });
 };
 
