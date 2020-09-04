4780b8ce9f0915d16b99e96cd14c75085f876527
Simon Farshid <Yonom@users.noreply.github.com>
2020-09-04 11:03:42 +0200

    Add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,7 @@ import './_app.css';
 /* Fonts */
 import 'typeface-roboto';
 
+import Head from 'next/head';
 import { SWRConfig } from 'swr';
 import { IonApp } from '@ionic/react';
 import { GlobalNotificationsProvider } from '../components/GlobalNotifications';
@@ -26,6 +27,18 @@ const fetcher = (...args) => fetch(...args).then(async (res) => {
 const App = ({ Component, pageProps }) => {
   return (
     <SWRConfig value={{ fetcher }}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#72993e" />
+
+        <meta name="msapplication-TileImage" content="/icons/mstile-150x150.png" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-TileColor" content="#31417a" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+
       <IonApp>
         <Component {...pageProps} />
         <GlobalNotificationsProvider />
