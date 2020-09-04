6a7c7babe21b5b9f17c1c0f00a2f32919a5ced70
Simon Farshid <Yonom@users.noreply.github.com>
2020-09-04 12:12:25 +0200

    PRAX-195 Add favicon (#176)
    * Add favicon
    
    * Adjust manifeset tab size
    
    * Add browserconfig
    
    * Fix mstile file path
    
    * Add apple PWA tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,7 @@ import './_app.css';
 /* Fonts */
 import 'typeface-roboto';
 
+import Head from 'next/head';
 import { SWRConfig } from 'swr';
 import { IonApp } from '@ionic/react';
 import { GlobalNotificationsProvider } from '../components/GlobalNotifications';
@@ -26,6 +27,27 @@ const fetcher = (...args) => fetch(...args).then(async (res) => {
 const App = ({ Component, pageProps }) => {
   return (
     <SWRConfig value={{ fetcher }}>
+      <Head>
+        <meta name="application-name" content="Correctly" />
+
+        {/* Icons & Manifest */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#72993e" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Colors */}
+        <meta name="msapplication-TileColor" content="#31417a" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Apple PWA */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Correctly" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="format-detection" content="telephone=no" />
+      </Head>
+
       <IonApp>
         <Component {...pageProps} />
         <GlobalNotificationsProvider />
