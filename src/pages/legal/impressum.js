ca5a6dc648efe53cca8a72e49587b88af2cf71d3
lionel lingnau <lionel.lingnau@hotmail.com>
2020-09-14 13:17:03 +0200

    Impressum & Privacy Page
    Impressum und Privacy Page erstellt

---
+++ src/pages/legal/impressum.js
@@ -0,0 +1,11 @@
+import AppPage from '../components/AppPage';
+
+const impressum = () => {
+  return (
+    <AppPage title="Impressum" footer="Correctly">
+      content goes here...
+    </AppPage>
+  );
+};
+
+export default impressum;
---
+++ src/pages/legal/privacy.js
@@ -0,0 +1,11 @@
+import AppPage from '../components/AppPage';
+
+const privacy = () => {
+  return (
+    <AppPage title="Privacy" footer="Correctly">
+      content goes here...
+    </AppPage>
+  );
+};
+
+export default privacy;
