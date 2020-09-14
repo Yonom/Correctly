168f17e1cf25b41987a6f88c41c6405cd2449b40
Luca <luca.lenhard@web.de>
2020-09-14 22:14:17 +0200

    Bug fixes

--- src/services/api/database/homework.js
+++ src/services/api/database/homework.js
@@ -70,7 +70,7 @@ export default function insertHomework(
   modelSolution,
   evaluationScheme,
 ) {
-  const queryText = 'INSERT INTO homeworks(homeworkName, maxReachablePoints, requireCorrectingDocumentationFile, evaluationVariant, correctionVariant, correctionValidation, samplesize, threshold, solutionAllowedFormats, correctionAllowedFormats, doingStart, doingEnd, correctingStart, correctingEnd, exerciseAssignment, modelSolution, evaluationScheme,) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *';
+  const queryText = 'INSERT INTO homeworks(homeworkName, maxReachablePoints, requireCorrectingDocumentationFile, evaluationVariant, correctionVariant, correctionValidation, samplesize, threshold, solutionAllowedFormats, correctionAllowedFormats, doingStart, doingEnd, correctingStart, correctingEnd, exerciseAssignment, modelSolution, evaluationScheme) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *';
   // eslint-disable-next-line no-console
   console.log('HALLO FILE 3');
   const params = [
@@ -88,9 +88,9 @@ export default function insertHomework(
     doingEnd,
     correctingStart,
     correctingEnd,
-    exerciseAssignment,
-    modelSolution,
-    evaluationScheme,
+    [exerciseAssignment],
+    [modelSolution],
+    [evaluationScheme],
   ];
   return databaseQuery(queryText, params);
 }
