064fa61c1ed05ff68649f1027b5fba6c1bd168e1
lionel lingnau <lionel.lingnau@hotmail.com>
2020-09-14 16:59:17 +0200

    Impressum content
    Impressum mit content gefüllt

--- src/pages/legal/impressum.js
+++ src/pages/legal/impressum.js
@@ -1,9 +1,32 @@
-import AppPage from '../components/AppPage';
+import { IonCardContent, IonItem, IonLabel } from '@ionic/react';
+import AppPage from '../../components/AppPage';
+import IonCenterContent from '../../components/IonCenterContent';
 
 const impressum = () => {
   return (
     <AppPage title="Impressum" footer="Correctly">
-      content goes here...
+      <IonCenterContent>
+        <IonCardContent>
+
+          <IonItem>
+            <IonLabel>
+              <b>Simon Farschid</b>
+            <p>Friedberger Landstraße 297</p>
+          <p>60389 Frankfurt</p>
+          <p>E-Mail- Adresse: simon.farshid@fs-students.de</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>
+            <b>Online-Streitbeilegung</b>
+            <p>Die Europäische Kommission stellt unterhttp://ec.europa.eu/consumers/odr/ eine Plattform zuraußergerichtlichen Online-Streitbeilegung (sog. OS-Plattform) bereit. Wir weisen darauf hin, dass wir an einem Streitbeilegungsverfahren vor einer Verbraucherstreitschlichtungsstelle nicht teilnehmen.
+          </p>
+            </IonLabel>
+          </IonItem>
+
+        </IonCardContent>
+      </IonCenterContent>
     </AppPage>
   );
 };
