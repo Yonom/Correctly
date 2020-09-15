d0f59c6ab6a3980a6134d430fde6a527015c998a
JoSch <JoSch@github.com>
2020-09-15 13:53:07 +0200

    Update impressum.js

--- src/pages/legal/impressum.js
+++ src/pages/legal/impressum.js
@@ -10,18 +10,19 @@ const impressum = () => {
 
           <IonItem>
             <IonLabel>
-              <b>Simon Farschid</b>
-            <p>Friedberger Landstraße 297</p>
-          <p>60389 Frankfurt</p>
-          <p>E-Mail- Adresse: simon.farshid@fs-students.de</p>
+              <b>Simon Farshid</b>
+              <p>Friedberger Landstraße 297</p>
+              <p>60389 Frankfurt</p>
+              <p>E-Mail- Adresse: simon.farshid@fs-students.de</p>
             </IonLabel>
           </IonItem>
 
           <IonItem>
             <IonLabel>
-            <b>Online-Streitbeilegung</b>
-            <p>Die Europäische Kommission stellt unterhttp://ec.europa.eu/consumers/odr/ eine Plattform zuraußergerichtlichen Online-Streitbeilegung (sog. OS-Plattform) bereit. Wir weisen darauf hin, dass wir an einem Streitbeilegungsverfahren vor einer Verbraucherstreitschlichtungsstelle nicht teilnehmen.
-          </p>
+              <b>Online-Streitbeilegung</b>
+              <p>
+                Die Europäische Kommission stellt unterhttp://ec.europa.eu/consumers/odr/ eine Plattform zuraußergerichtlichen Online-Streitbeilegung (sog. OS-Plattform) bereit. Wir weisen darauf hin, dass wir an einem Streitbeilegungsverfahren vor einer Verbraucherstreitschlichtungsstelle nicht teilnehmen.
+              </p>
             </IonLabel>
           </IonItem>
 
