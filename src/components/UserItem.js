2a2c1e03dd4be1ad651b74fcc001617ce85fcff6
rmr23 <robrinn@web.de>
2020-09-28 21:55:21 +0200

    Updated studentid impelementation

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -3,10 +3,15 @@ import { useState } from 'react';
 
 const UserItem = ({ user, id, selected, roleString, onCheck }) => {
   const [checked, setChecked] = useState(selected);
+
+  const idString = user.studentid !== undefined && user.studentid !== null
+    ? `(id: ${user.studentid})`
+    : '';
+
   return (
     <div style={{ width: '100%' }}>
       <IonItem key={id}>
-        <IonLabel>{`${user.firstname} ${user.lastname} ${user?.studentid}`}</IonLabel>
+        <IonLabel>{`${user.firstname} ${user.lastname} ${idString}`}</IonLabel>
         <IonCheckbox checked={checked} onIonChange={(e) => onCheck(e, user, setChecked, roleString)} />
       </IonItem>
     </div>
