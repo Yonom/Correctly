d6cd6143a30a26a537a9657587a35e4d799ef1b6
Skywalker360 <33625834+Skywalker360@users.noreply.github.com>
2020-09-30 17:37:01 +0200

    ES-Lint fixes

--- src/services/courses.js
+++ src/services/courses.js
@@ -3,7 +3,3 @@ import useSWR from 'swr';
 export const useMyEditableCourses = () => {
   return useSWR('/api/courses/myEditable');
 };
-
-export const useUsers = () => {
-  return useSWR('/api/courses/getUsers');
-};
