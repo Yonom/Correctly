99ae6cc87c7cbc587f499aa4af11a5e736f04295
Simon Farshid <Yonom@users.noreply.github.com>
2020-10-04 19:37:22 +0200

    Implement getPercentageGrade

---
+++ src/utils/percentageGradeConst.js
@@ -0,0 +1,11 @@
+export const NOT_DONE = 'not-done';
+export const WRONG = 'wrong';
+export const RIGHT = 'right';
+export const EFFORT = true;
+export const NO_EFFORT = false;
+
+export const EFFORTS = 'efforts';
+export const ZERO_TO_ONE_HUNDRED = 'zeroToOnehundred';
+export const NOT_WRONG_RIGHT = 'notWrongRight';
+export const ITS_OK_TO_FAIL = 'itsOkayToFail';
+export const POINTS = 'points';
