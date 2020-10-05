52906d4c13e10a2f1d0d53dedf491069bfcb370a
Malte Blank <malte.blank@fs-students.de>
2020-10-05 12:55:35 +0200

    Update viewCoursePage
    Added hyperlink to viewHomework Page for SHOW buttons

--- src/pages/courses/[courseId].js
+++ src/pages/courses/[courseId].js
@@ -73,7 +73,7 @@ const ViewCoursePage = () => {
       <div style={{ width: '100%' }}>
         <IonItem key={h.homeworkId}>
           <IonLabel position="float">{`${h.homeworkname}`}</IonLabel>
-          <IonButton position="float">SHOW</IonButton>
+          <IonButton position="float" href={`/homeworks/${h.homeworkId}`}>SHOW</IonButton>
         </IonItem>
       </div>
     );
