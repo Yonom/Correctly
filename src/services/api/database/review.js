ccaf0ec0d5de00dd90c5d3e4321abf979896c5d4
Simon Farshid <Yonom@users.noreply.github.com>
2020-10-06 14:38:16 +0200

    Fix distribution of homeworks not distributing

--- src/services/api/database/review.js
+++ src/services/api/database/review.js
@@ -1,53 +1,57 @@
 import { databaseTransaction } from '.';
 
+const createParamsForDistributedHomeworks = (solutionList, correctingVariant) => {
+  // convert correctingVariant into Integer
+  let correctors;
+  if (correctingVariant === 'correct-one') {
+    correctors = 1;
+  } else if (correctingVariant === 'correct-two') {
+    correctors = 2;
+  } else {
+    throw new Error(`too many correctors (${correctingVariant}) in Homework`);
+  }
+
+  const params = [];
+  // Loop for all users in list with an index lower than length of list - correctingVariant
+  for (let i = 0; i < solutionList.length - correctors; i++) {
+    for (let j = 1; j <= correctors; j++) {
+      params.push([solutionList[i].userid, solutionList[i + j].solutionid]);
+    }
+  }
+
+  // Loop for all users not covered by the previous loop. This splitting is necessary to avoid null-pointer-exceptions
+  for (let i = solutionList.length - correctors; i < solutionList.length; i++) {
+    for (let j = 1; j <= correctors; j++) {
+      let id = i + j;
+      if (id >= solutionList.length) {
+        id -= solutionList.length;
+      }
+      params.push([solutionList[i].userid, solutionList[id].solutionid]);
+    }
+  }
+};
+
 /**
  * text
  *
- * @param {varchar(64)} userList
- * @param {number} solutionList
- * @param {varchar(64)} correctingVariant
+ * @param {string[]} solutionList
+ * @param {string[]} notDoneUserList
+ * @param {string} correctingVariant
  * @param {string} homeworkId
  */
-export async function createReview(userList, solutionList, correctingVariant, homeworkId) {
+export async function createReview(solutionList, notDoneUserList, correctingVariant, homeworkId) {
   return databaseTransaction(async (client) => {
-    let correctors = 0;
-
-    // convert correctingVariant into Integer
-    if (correctingVariant === 'correct-one') {
-      correctors = 1;
-    } else if (correctingVariant === 'correct-two') {
-      correctors = 2;
-    } else {
-      throw new Error(`too many correctors (${correctingVariant}) in Homework (${homeworkId})`);
-    }
-
-    const queryText1 = 'INSERT INTO reviews(userid, solutionid) VALUES($1, $2)';
-
-    // Loop for all users in list with an index lower than length of list - correctingVariant
-    for (let i = 0; i < userList.length - correctors; i++) {
-      for (let j = 1; j <= correctors; j++) {
-        const params1 = [userList[i], solutionList[i + j]];
-        client.databaseQuery(queryText1, params1);
-      }
-    }
-
-    // Loop for all users not covered by the previous loop. This splitting is necessary to avoid null-pointer-exceptions
-    for (let i = userList.length - correctors; i < userList.length; i++) {
-      for (let j = 1; j <= correctors; j++) {
-        let id = i + j;
-        if (id >= userList.length) {
-          id -= userList.length;
-        }
-        const params1 = [userList[i], solutionList[id]];
-        client.databaseQuery(queryText1, params1);
-      }
+    const queryText1 = 'INSERT INTO reviews(userid, solutionid, issystemreview) VALUES($1, $2, $3)';
+    const params1Collection = createParamsForDistributedHomeworks(solutionList, correctingVariant);
+    for (const params1 of params1Collection) {
+      await client.query(queryText1, params1);
     }
 
     // Upadting the homework
-    const queryText2 = `UPDATE homeworks
+    const queryText3 = `UPDATE homeworks
     SET distributedReviews = 1
     WHERE id = $1`;
-    const params2 = [homeworkId];
-    client.databaseQuery(queryText2, params2);
+    const params3 = [homeworkId];
+    await client.databaseQuery(queryText3, params3);
   });
 }
