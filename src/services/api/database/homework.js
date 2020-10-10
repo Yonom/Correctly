c03d04c94521dc177cc010c707482459cde3be72
Simon Busse <simon.busse@fs-students.de>
2020-10-10 18:30:21 +0200

    Added ManageHomeworks

--- src/services/api/database/homework.js
+++ src/services/api/database/homework.js
@@ -161,16 +161,25 @@ export const selectHomework = async (homeworkId) => {
 
 export const selectEditableHomeworksForUser = async (userId) => {
   const queryText = `
-    SELECT homeworks.id, homeworkname, courses.yearcode as yearcode, courses.title as title 
+  SELECT homeworks.id as id, homeworkname, courses.yearcode as yearcode, courses.title as title, users.firstname as firstname, users.lastname as lastname, doingstart, doingend, correctingstart, correctingend
     FROM homeworks
     INNER JOIN courses ON homeworks.courseid = courses.id
-    INNER JOIN users ON users.userid = attends.userid
-    WHERE users.userid = $1
-    AND isactive AND isemailverified 
-    AND (islecturer OR ismodulecoordinator OR isstudent)
+    INNER JOIN users ON homeworks.creator = users.userid
+    WHERE homeworks.creator = $1
   `;
   const params = [userId];
   return databaseQuery(queryText, params);
+};
+
+export const selectAllHomeworks = async () => {
+  const queryText = `
+    SELECT homeworks.id as id, homeworkname, courses.yearcode as yearcode, courses.title as title, users.firstname as firstname, users.lastname as lastname, doingstart, doingend, correctingstart, correctingend
+    FROM homeworks
+    INNER JOIN courses ON homeworks.courseid = courses.id
+    INNER JOIN users ON homeworks.creator = users.userid
+  `;
+  const params = [];
+  return await databaseQuery(queryText, params);
 };
 
 export const selectHomeworkForUser = async (homeworkId, userId) => {
