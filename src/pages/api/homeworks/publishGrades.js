e346880b038e478f436c35872300a131dd091442
winnlittstruck <33479212+winnlittstruck@users.noreply.github.com>
2020-11-09 11:00:26 +0100

    PRAX-248 publish grades button (#335)
    * Button added
    
    * implemented check on wheather grades have been published and added "gradesPublished" property to homework object
    
    * changed name to gradesPublished
    
    * implemented API call and db update function to publish grades
    
    * Update add.js
    
    * Update publishGrades.js
    
    * Update [homeworkId].js
    
    Co-authored-by: Carl Luippold <carl.luippold@manager24.de>

---
+++ src/pages/api/homeworks/publishGrades.js
@@ -0,0 +1,33 @@
+import handleRequestMethod from '../../../utils/api/handleRequestMethod';
+import authMiddleware from '../../../utils/api/auth/authMiddleware';
+import { isLecturer, isSuperuser } from '../../../utils/auth/role';
+import { updateHomeworkGradesPublished, selectEditableHomeworksForUser } from '../../../services/api/database/homework';
+
+const publishGradesAPI = async (req, res, { role, userId }) => {
+  // Prüfung auf POST-Request
+  await handleRequestMethod(req, res, 'POST');
+  // checking wheather the user is allowed to edit this homework and therefore
+  // allowed to publish grades
+  const myEditables = await selectEditableHomeworksForUser(userId, isSuperuser(role));
+  const { homeworkId } = req.body || {};
+  let isAllowed = false;
+  if (!isLecturer(role) && !isSuperuser(role)) {
+    return res.status(400).json({ code: 'homework/publishing-grades-not-allowed' });
+  }
+  for (let i = 0; i < myEditables.rows.length; i++) {
+    if (homeworkId === myEditables.rows[i].id) { isAllowed = true; }
+  }
+  if (!isAllowed) {
+    return res.status(400).json({ code: 'homework/publishing-grades-not-allowed' });
+  }
+
+  // publish grades of homework as Transaction
+  try {
+    await updateHomeworkGradesPublished(homeworkId);
+    return res.status(200).json({ });
+  } catch (err) {
+    return res.status(500);
+  }
+};
+
+export default authMiddleware(publishGradesAPI);
--- src/services/api/database/homework.js
+++ src/services/api/database/homework.js
@@ -220,6 +220,7 @@ export const selectHomeworkForUser = async (homeworkId, userId, isSuperuser) => 
       homeworks.taskfilenames,
       homeworks.samplesolutionfilenames,
       homeworks.evaluationschemefilenames,
+      homeworks.gradespublished,
       courses.yearcode, 
       courses.title
     FROM homeworks 
@@ -353,4 +354,14 @@ export const selectHomeworksAndGradesForCourseAndUser = async (courseId, userId)
   `;
   const params = [courseId, userId];
   return await databaseQuery(queryText, params);
+};
+
+export const updateHomeworkGradesPublished = (homeworkId) => {
+  return databaseTransaction(async (client) => {
+    const queryTextPublishGrades = 'UPDATE homeworks SET gradespublished = true WHERE id = $1';
+    const paramsPublishGrades = [homeworkId];
+    const res = await client.query(queryTextPublishGrades, paramsPublishGrades);
+
+    return res;
+  });
 };
