69aec86678195533fdc2d551bde7f8ec986277a3
Simon Farshid <Yonom@users.noreply.github.com>
2020-11-11 20:06:40 +0100

    Make sentry work for API calls

--- src/services/sentry.js
+++ src/services/sentry.js
@@ -3,34 +3,32 @@ import * as Sentry from '@sentry/node';
 import { RewriteFrames } from '@sentry/integrations';
 
 export const init = () => {
-  if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-    const integrations = [];
-    if (
-      process.env.NEXT_IS_SERVER === 'true'
+  const integrations = [];
+  if (
+    process.env.NEXT_IS_SERVER === 'true'
       && process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
-    ) {
-      // For Node.js, rewrite Error.stack to use relative paths, so that source
-      // maps starting with ~/_next map to files in Error.stack with path
-      // app:///_next
-      integrations.push(
-        new RewriteFrames({
-          iteratee: (frame) => {
-            frame.filename = frame.filename.replace(
-              process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
-              'app:///',
-            );
-            frame.filename = frame.filename.replace('.next', '_next');
-            return frame;
-          },
-        }),
-      );
-    }
-
-    Sentry.init({
-      enabled: process.env.NODE_ENV === 'production',
-      integrations,
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-      release: process.env.NEXT_PUBLIC_COMMIT_SHA,
-    });
+  ) {
+    // For Node.js, rewrite Error.stack to use relative paths, so that source
+    // maps starting with ~/_next map to files in Error.stack with path
+    // app:///_next
+    integrations.push(
+      new RewriteFrames({
+        iteratee: (frame) => {
+          frame.filename = frame.filename.replace(
+            process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
+            'app:///',
+          );
+          frame.filename = frame.filename.replace('.next', '_next');
+          return frame;
+        },
+      }),
+    );
   }
+
+  Sentry.init({
+    enabled: process.env.NODE_ENV === 'production',
+    integrations,
+    dsn: 'https://4fd075a0296c4407a14fb89237c13cb3@o475139.ingest.sentry.io/5512749',
+    release: process.env.VERCEL_GITHUB_COMMIT_SHA,
+  });
 };
