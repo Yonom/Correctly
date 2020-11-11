3c0ce20cb9f6c0d3679d76684508b947cae6e760
Simon Farshid <Yonom@users.noreply.github.com>
2020-11-11 16:18:33 +0100

    Revert "PRAX-248 publish grades button (#335)"
    This reverts commit e346880b038e478f436c35872300a131dd091442.

--- src/services/homeworks.js
+++ src/services/homeworks.js
@@ -104,7 +104,3 @@ export const useMyHomeworks = () => {
 export const useMyEditableHomeworks = () => {
   return useSWR('/api/homeworks/myEditable');
 };
-
-export const homeworksPublishGrades = async (homeworkId) => {
-  return await fetchPost('/api/homeworks/publishGrades', { homeworkId });
-};
