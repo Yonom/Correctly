507928b91d414553e52b046651cf987a00eb3b8d
rmr23 <robrinn@web.de>
2020-11-16 20:06:21 +0100

    Plagiarism Algo Check

---
+++ src/pages/plagiarism/checkAlgo.js
@@ -0,0 +1,45 @@
+/* Ionic imports */
+import { IonButton, IonCol, IonGrid, IonRow, IonInput, IonTextarea } from '@ionic/react';
+import { useState } from 'react';
+import similarity from 'string-cosine-similarity';
+import stringSimilarity from 'string-similarity';
+import Levenshtein from 'levenshtein';
+
+import AppPage from '../../components/AppPage';
+
+const PlagiarismIsADancer = () => {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+
+  const calculateSimilarity = () => {
+    const cosineSimilarity = similarity(text1, text2);
+    const levenshteinDistance = new Levenshtein(text1, text2);
+    const levenshteinLength = Math.max(text1.length, text2.length);
+    const levenshteinSimilarity = 1 - (levenshteinDistance.distance / levenshteinLength);
+    const diceSimilarity = stringSimilarity.compareTwoStrings(text1, text2);
+    alert(`Similarity (cosine): ${cosineSimilarity}% \nSimilarity (levenshtein): ${levenshteinSimilarity}% \nSimilarity (dice): ${diceSimilarity}% `);
+  };
+
+  return (
+    <AppPage title="Plagiarism Is A Dancer">
+      <IonGrid>
+        <IonRow>
+          <IonCol size="6">
+            <IonTextarea autoGrow value={text1} type="text" name="text1" onIonChange={(e) => { setText1(e.detail.value); }} placeholder="Hier Text 1 eingeben 🤏">
+              Text1
+            </IonTextarea>
+
+          </IonCol>
+          <IonCol size="6">
+            <IonTextarea autoGrow value={text2} type="text" name="text2" onIonChange={(e) => { setText2(e.detail.value); }} placeholder="Hier Text 2 eingeben 😂👌">
+              Text2
+            </IonTextarea>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonButton onClick={calculateSimilarity}>calc</IonButton>
+    </AppPage>
+  );
+};
+
+export default PlagiarismIsADancer;
