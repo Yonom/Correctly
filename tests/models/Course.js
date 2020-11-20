f1b4cc390e9aade798e55206edf7bb0a6edbe30d
Simon Farshid <Yonom@users.noreply.github.com>
2020-11-20 14:12:43 +0100

    Add support to add attendees and hoemworks to courses

--- tests/models/Course.js
+++ tests/models/Course.js
@@ -8,6 +8,48 @@ class Course {
   constructor(obj) {
     Object.assign(this, obj);
   }
+
+  async addAttendee({
+    userid,
+    courseid = this.id,
+    isstudent,
+    islecturer,
+    ismodulecoordinator,
+  }) {
+    await insertInto('attends', userid, courseid, isstudent, islecturer, ismodulecoordinator);
+  }
+
+  async addHomework({
+    homeworkname = 'Test Homework',
+    courseid = this.id,
+    maxreachablepoints = 10,
+    evaluationvariant = EFFORTS,
+    reviewercount = ONE_REVIEWER,
+    auditors = AUDIT_BY_LECTURERS,
+    samplesize = 0,
+    threshold = -1,
+    solutionallowedformats = [TEXTFIELD],
+    reviewallowedformats = [TEXTFIELD],
+    solutionstart = moment(),
+    solutionend = moment().add(1, 'day'),
+    reviewstart = moment().add(1, 'day'),
+    reviewend = moment().add(2, 'days'),
+    taskfiles = [null],
+    taskfilenames = [null],
+    samplesolutionfiles = [null],
+    samplesolutionfilenames = [null],
+    evaluationschemefiles = [null],
+    evaluationschemefilenames = [null],
+    creator = 'TRZhASY8Figbt9YKoG0rvP4XOCE3', // Dozent Eins
+    creationdate = new Date(),
+    hasdistributedreviews = false,
+    hasdistributedaudits = false,
+    gradespublished = false,
+  } = {}) {
+    return new Homework(
+      await insertInto('homeworks', homeworkname, courseid, maxreachablepoints, evaluationvariant, reviewercount, auditors, samplesize, threshold, solutionallowedformats, reviewallowedformats, solutionstart, solutionend, reviewstart, reviewend, taskfiles, taskfilenames, samplesolutionfiles, samplesolutionfilenames, evaluationschemefiles, evaluationschemefilenames, creator, creationdate, hasdistributedreviews, hasdistributedaudits, gradespublished),
+    );
+  }
 }
 
 const deleteCourse = async ({ id }) => {
---
+++ tests/models/Homework.js
@@ -0,0 +1,5 @@
+export default class Homework {
+  constructor(obj) {
+    Object.assign(this, obj);
+  }
+}
