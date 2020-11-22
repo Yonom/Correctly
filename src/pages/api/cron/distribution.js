0ce12cc47e265bba3a3a4240ec6b0ac42061cf79
Carl Luippold <carl.luippold@manager24.de>
2020-11-22 17:58:03 +0100

    Update distribution.js

--- src/pages/api/cron/distribution.js
+++ src/pages/api/cron/distribution.js
@@ -114,7 +114,7 @@ const distributeAudits = async () => {
     // Zufälliges hinzufügen von x Werten (einmalig) zur ReviewAuditIndexlist
       for (let i = 0; i < samplesToCreate; i++) {
         const number = Math.round(Math.floor(Math.random() * solutionQuery.rows.length));
-        if (reviewAudit.includes(solutionQuery.rows[number].id)) {
+        if (reviewAudit.includes(solutionQuery.rows[number].id || notStudentReview.includes(solutionQuery.rows[number].id))) {
           i -= 1;
         } else {
           reviewAudit.push(solutionQuery.rows[number].id);
