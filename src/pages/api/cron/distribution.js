9e39425b5f6a1a24b2a4d3796595b12c8991f528
Carl Luippold <carl.luippold@manager24.de>
2020-11-23 21:29:31 +0100

    Update distribution.js

--- src/pages/api/cron/distribution.js
+++ src/pages/api/cron/distribution.js
@@ -50,81 +50,88 @@ const distributeAudits = async () => {
   for (const homework of homeworkQuery.rows) {
     const notDoneUsersQuery = await selectUsersWithoutReview(homework.id, homework.courseid);
     const solutionQuery = await selectSolutions(homework.id);
-    const { samplesize } = homework; // <- Hier samplesize definieren
-    const { threshold } = homework; // <- Hier samplesize definieren
-    const reviewerCount = homework.reviewercount;
-    const notDoneUsers = notDoneUsersQuery.rows;
 
-    let alpha;
+    if (solutionQuery.rows.length > 2) {
+      const { samplesize } = homework; // <- Hier samplesize definieren
+      const { threshold } = homework; // <- Hier samplesize definieren
+      const reviewerCount = homework.reviewercount;
+      const notDoneUsers = notDoneUsersQuery.rows;
 
-    if (threshold && threshold !== THRESHOLD_NA) {
-      alpha = threshold / 100;
-    }
+      let alpha;
 
-    const reviewAudit = [];
-    const reasonList = [];
+      if (threshold && threshold !== THRESHOLD_NA) {
+        alpha = threshold / 100;
+      }
 
-    // Rausfiltern der Missing reviews, not done users
-    for (const solution of solutionQuery.rows) {
-      const reviewQuery = await selectReviewsForSolution(solution.id);
+      const reviewAudit = [];
+      const reasonList = [];
 
-      const user = { userid: solution.userid };
+      // Rausfiltern der Missing reviews, not done users
+      for (const solution of solutionQuery.rows) {
+        const reviewQuery = await selectReviewsForSolution(solution.id);
+        if (reviewQuery.rows.length !== 0) {
+          const user = { userid: solution.userid };
+          console.log(notDoneUsers);
+          console.log(user);
+          console.log(JSON.stringify(notDoneUsers).includes(JSON.stringify(user)));
 
-      if (JSON.stringify(notDoneUsers).includes(JSON.stringify(user))) {
-        reviewAudit.push(solution.id);
-        reasonList.push(AUDIT_REASON_DID_NOT_SUBMIT_REVIEW);
-      } else {
-        for (const review of reviewQuery.rows) {
-          if (!review.issubmitted && !reviewAudit.includes(solution.id)) {
+          if (JSON.stringify(notDoneUsers).includes(JSON.stringify(user))) {
             reviewAudit.push(solution.id);
-            reasonList.push(AUDIT_REASON_MISSING_REVIEW_SUBMISSION);
+            reasonList.push(AUDIT_REASON_DID_NOT_SUBMIT_REVIEW);
+          } else {
+            for (const review of reviewQuery.rows) {
+              if (!review.issubmitted && !reviewAudit.includes(solution.id)) {
+                reviewAudit.push(solution.id);
+                reasonList.push(AUDIT_REASON_MISSING_REVIEW_SUBMISSION);
+              }
+            }
           }
         }
       }
-    }
 
-    // Wenn 2 Bewerter werden die reviews auf threshold geprüft falls dieser nicht N/A
-    if (reviewerCount === TWO_REVIEWERS && threshold !== THRESHOLD_NA.toString()) {
-      for (const solution of solutionQuery.rows) {
+      // Wenn 2 Bewerter werden die reviews auf threshold geprüft falls dieser nicht N/A
+      if (reviewerCount === TWO_REVIEWERS && threshold !== THRESHOLD_NA.toString()) {
+        for (const solution of solutionQuery.rows) {
         // Prüfen ob solution nicht bereits im Audit ist (MISSING Review/ NOT SUbmittet) oder EInen Lecturerreview enthält
-        if (!reviewAudit.includes(solution.id)) {
-          const reviewQuery = await selectReviewsForSolution(solution.id);
-          const grades = [];
-          grades.push(reviewQuery.rows[0].percentagegrade);
-          grades.push(reviewQuery.rows[1].percentagegrade);
+          if (!reviewAudit.includes(solution.id)) {
+            const reviewQuery = await selectReviewsForSolution(solution.id);
+            const grades = [];
+            grades.push(reviewQuery.rows[0].percentagegrade);
+            grades.push(reviewQuery.rows[1].percentagegrade);
 
-          if (homework.evaluationvariant === ZERO_TO_ONE_HUNDRED || homework.evaluationvariant === POINTS) {
+            if (homework.evaluationvariant === ZERO_TO_ONE_HUNDRED || homework.evaluationvariant === POINTS) {
             // Zahlen threshold
-            const delta = Math.abs(grades[0] - grades[1]) / 100;
+              const delta = Math.abs(grades[0] - grades[1]) / 100;
 
-            if (delta >= alpha) {
+              if (delta >= alpha) {
+                reviewAudit.push(solution.id);
+                reasonList.push(AUDIT_REASON_THRESHOLD);
+              }
+            } else if (grades[0] !== grades[1]) {
+            // Nominaler threshold
               reviewAudit.push(solution.id);
               reasonList.push(AUDIT_REASON_THRESHOLD);
             }
-          } else if (grades[0] !== grades[1]) {
-            // Nominaler threshold
-            reviewAudit.push(solution.id);
-            reasonList.push(AUDIT_REASON_THRESHOLD);
           }
         }
       }
-    }
 
-    const samplesToCreate = Math.min(samplesize, solutionQuery.rows.length - reviewAudit.length);
+      const samplesToCreate = Math.min(samplesize, solutionQuery.rows.length - reviewAudit.length);
 
-    if (samplesize > 0) {
-    // Zufälliges hinzufügen von x Werten (einmalig) zur ReviewAuditIndexlist
-      for (let i = 0; i < samplesToCreate; i++) {
-        const number = Math.round(Math.floor(Math.random() * solutionQuery.rows.length));
-        if (reviewAudit.includes(solutionQuery.rows[number].id)) {
-          i -= 1;
-        } else {
-          reviewAudit.push(solutionQuery.rows[number].id);
-          reasonList.push(AUDIT_REASON_SAMPLESIZE);
+      if (samplesize > 0) {
+        // Zufälliges hinzufügen von x Werten (einmalig) zur ReviewAuditIndexlist
+        for (let i = 0; i < samplesToCreate; i++) {
+          const number = Math.round(Math.floor(Math.random() * solutionQuery.rows.length));
+          if (reviewAudit.includes(solutionQuery.rows[number].id)) {
+            i -= 1;
+          } else {
+            reviewAudit.push(solutionQuery.rows[number].id);
+            reasonList.push(AUDIT_REASON_SAMPLESIZE);
+          }
         }
       }
+      await createAudits(reviewAudit, reasonList, notDoneUsers, homework.id);
     }
-    await createAudits(reviewAudit, reasonList, notDoneUsers, homework.id);
   }
 };
 
