033d3a82cd9117786d40e50b0b8d03ea8d44902a
Simon Farshid <Yonom@users.noreply.github.com>
2020-11-27 11:05:15 +0100

    Update package and fix dashboard refresh issue

--- src/pages/api/cron/dashboard.js
+++ src/pages/api/cron/dashboard.js
@@ -2,6 +2,9 @@ import { databaseQuery } from '../../../services/api/database';
 import withSentry from '../../../utils/api/withSentry';
 
 const dashboardRefreshAPI = async (req, res) => {
+  await databaseQuery('REFRESH MATERIALIZED VIEW homeworkhistory');
+  await databaseQuery('REFRESH MATERIALIZED VIEW solutionhistory');
+  await databaseQuery('REFRESH MATERIALIZED VIEW gradehistory');
   await databaseQuery('REFRESH MATERIALIZED VIEW homeworkhistorylong');
   await databaseQuery('REFRESH MATERIALIZED VIEW solutionhistorylong');
   await databaseQuery('REFRESH MATERIALIZED VIEW gradehistorylong');
